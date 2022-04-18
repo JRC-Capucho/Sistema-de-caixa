6fd23732721f99a9ad6dc3c96fc10a9ee782830f
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2022-04-18 00:48:13 -0300

    Complete

--- Caixa.cs
+++ Caixa.cs
@@ -5,20 +5,179 @@ namespace CaixaDeMercado
 {
     class Caixa
     {
+        private string nameProduct = "[Nome]=--------=[Preço]\n";
+        private double payFinal = 0;
+        private int limit;
+        private string personName;
+        private Db db = new Db();
         private MySqlDataReader? reader;
-       public static void Main(String[] args)
-       {    
-           Caixa caixa = new Caixa();
-           Db db = new Db();
-//           db.insertProduct(123,"mac",20.50,10);
-           caixa.reader = db.readerProduct(123);
-           while(caixa.reader.Read())
-           {
-           Console.WriteLine(Convert.ToString(caixa.reader["Name"])); 
-           Console.WriteLine(Convert.ToString(caixa.reader["Price"]));
-           Console.WriteLine(Convert.ToString(caixa.reader["Stock"]));
-           }
-           db.closesql();
-       }
+
+        public void loginSession()
+        {
+            System.Console.WriteLine("============================================\n");
+            System.Console.WriteLine("F f = Funcionario");
+            System.Console.WriteLine("G g = Gerente");
+            System.Console.WriteLine("\n============================================\n");
+            System.Console.WriteLine("Você e");
+            string aux = Console.ReadLine();
+
+            System.Console.WriteLine("\nDigite seu numero do crachá:");
+            int personId = Convert.ToInt32(Console.ReadLine());
+
+            switch (aux)
+            {
+                case "f":
+                case "F":
+                    personName = "[f] " + db.readerEmployer(personId);
+                    break;
+                case "g":
+                case "G":
+                    personName = "[g] " + db.readerManager(personId);
+                    break;
+            }
+        }
+
+        public void menuManager()
+        {
+            string aux;
+            int option;
+
+            do
+            {
+                System.Console.WriteLine("\nBem vindo {0}", personName.Substring(4));
+                System.Console.WriteLine("\n============================================\n");
+                System.Console.WriteLine("1 - Para adicionar novos produtos");
+                System.Console.WriteLine("2 - Para adicionar novos funcionarios");
+                option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        addProduct();
+                        break;
+
+                    case 2:
+                        addEmployer();
+                        break;
+                }
+
+                System.Console.WriteLine("\nS s = Sim\nN n = Não");
+                System.Console.WriteLine("+------------+");
+
+                System.Console.WriteLine("Deseja encerrar a sessão");
+                aux = Console.ReadLine();
+
+            } while (aux.Equals("n") || aux.Equals("N"));
+
+            System.Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadLine();
+        }
+
+        public void addEmployer()
+        {
+            System.Console.WriteLine("============================================\n");
+
+            System.Console.WriteLine("Digite o nome do novo Funcionario:");
+            string name = Console.ReadLine();
+
+            System.Console.WriteLine("\nDigite o numero do crachá:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            db.insertEmployer(id, name);
+            System.Console.WriteLine("\n============================================\n");
+
+        }
+
+        public void addProduct()
+        {
+            System.Console.WriteLine("============================================\n");
+            System.Console.WriteLine("Digite o nome do novo produto:");
+            string name = Console.ReadLine();
+
+            System.Console.WriteLine("\nDigite o preço do novo produto:");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            db.insertProduct(name, price);
+            System.Console.WriteLine("\n============================================\n");
+        }
+
+
+
+        public void menuEmployer()
+        {
+            string aux;
+            do
+            {
+                menu();
+                chooseProduct();
+
+                System.Console.WriteLine("\nS s = Sim\nN n = Não");
+                System.Console.WriteLine("+------------+");
+                System.Console.WriteLine("Deseja finalizar a compra:");
+                aux = Console.ReadLine();
+
+            } while (aux.Equals("n") || aux.Equals("N"));
+            payment();
+        }
+
+        public void menu()
+        {
+
+            System.Console.WriteLine("\nBem vindo {0}", personName.Substring(4));
+            limit = db.listProduct();
+        }
+
+        public void chooseProduct()
+        {
+            System.Console.WriteLine("Escolha uma das opções de cima:");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            if (option > limit)
+            {
+                System.Console.WriteLine("Opção invalida");
+            }
+            else
+            {
+                nameProduct += db.getNamePrice(option);
+                payFinal += db.getPrice(option);
+            }
+        }
+
+        public void payment()
+        {
+            System.Console.WriteLine("\n============================================\n");
+            System.Console.WriteLine("Atendido pelo Funcionario: {0}\n", personName.Substring(4));
+            System.Console.WriteLine(nameProduct);
+            System.Console.WriteLine("--------------------------------------------\n");
+            System.Console.WriteLine("Total a pagar: {0:F2}", payFinal);
+            System.Console.WriteLine("\nPressione qualquer tecla para sair...");
+            Console.ReadLine();
+        }
+
+
+        public static void Main(String[] args)
+        {
+            Caixa caixa = new Caixa();
+
+            caixa.loginSession();
+
+
+            if (caixa.personName.Contains("invalid"))
+            {
+                System.Console.WriteLine("Cracha invalido");
+            }
+            else
+            {
+                if (caixa.personName[1].Equals('f'))
+                {
+                    caixa.menuEmployer();
+                }
+                if (caixa.personName[1].Equals('g'))
+                {
+                    caixa.menuManager();
+                }
+            }
+        }
+
     }
 }
--- Db.cs
+++ Db.cs
@@ -6,34 +6,20 @@ namespace CaixaDeMercado
     public class Db
     {
         static string connectionString = "server=localhost; user=root; database=tecshop; port=3306; password=";
-        
+
         static MySqlConnection connection = new MySqlConnection(connectionString);
-        static MySqlCommand? command; 
+        static MySqlCommand? command;
         static MySqlDataReader? reader;
-        //static MySqlDataAdapter? adapter;
 
-        public void closesql()
+        private string addProduct = "insert into product (Name, Price) values (@name,@price)";
+
+        public void insertProduct(string name, double price)
         {
             try
             {
-                connection.Close();
-            }
-            catch (MySql.Data.MySqlClient.MySqlException ex)
-            {
-                System.Console.WriteLine("Erro " +ex.Message.ToString());   
-            }
-        }
-        private string addProduct = "insert into product (BarCode, Name, Price, Stock) values (@barcode,@name,@price,@stock)";
-
-        public void insertProduct(int barCode, string name, double price, int stock)
-        {
-            try
-            {
-                command = new MySqlCommand(addProduct,connection);
-                command.Parameters.AddWithValue("@barcode", barCode);
+                command = new MySqlCommand(addProduct, connection);
                 command.Parameters.AddWithValue("@name", name);
                 command.Parameters.AddWithValue("@price", price);
-                command.Parameters.AddWithValue("@stock", stock);
 
                 connection.Open();
                 command.ExecuteNonQuery();
@@ -41,32 +27,111 @@ namespace CaixaDeMercado
             }
             catch (MySql.Data.MySqlClient.MySqlException ex)
             {
-                System.Console.WriteLine("Erro " +ex.Message.ToString());   
+                System.Console.WriteLine("Erro " + ex.Message.ToString());
+            }
+            finally
+            {
+                System.Console.WriteLine("Produto cadastrado com sucesso!");
+                connection.Close();
+            }
+        }
+
+        private string checkListProduct = "select * from product";
+
+        public int listProduct()
+        {
+            int limit = 0;
+            try
+            {
+                command = new MySqlCommand(checkListProduct, connection);
+
+                connection.Open();
+
+                System.Console.WriteLine("\n============================================\n");
+                string data = "[Opções]=-=[Nome]=-----=[Preço]\n";
+
+                reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    data += "    " + Convert.ToString(reader["id"]) + "\t  " + Convert.ToString(reader["Name"]) + "\t" + Convert.ToString(reader["Price"]) + "\n";
+                    limit++;
+                }
+                System.Console.WriteLine(data);
+                System.Console.WriteLine("============================================\n");
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                System.Console.WriteLine("Erro " + ex.Message.ToString());
             }
             finally
             {
                 connection.Close();
             }
+            return limit;
+
         }
 
-        private string checkProduct = "select * from product where BarCode=@barcode";
+        private string namePriceProduct = "select name, price from product where id=@id;";
 
-        public MySqlDataReader readerProduct(int barcode)
+        public string getNamePrice(int id)
         {
+            string data = "invalid";
+
             try
             {
-                command = new MySqlCommand(checkProduct, connection);
-                command.Parameters.AddWithValue("@barcode", barcode);
+                command = new MySqlCommand(namePriceProduct, connection);
+                command.Parameters.AddWithValue("@id", id);
 
                 connection.Open();
 
                 reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    data = Convert.ToString(reader["Name"]) + "\t" + Convert.ToString(reader["Price"]) + "\n";
+                }
             }
             catch (MySql.Data.MySqlClient.MySqlException ex)
             {
-                System.Console.WriteLine("Erro " +ex.Message.ToString());   
+                System.Console.WriteLine("Erro " + ex.Message.ToString());
             }
-            return reader;
+            finally
+            {
+                connection.Close();
+            }
+            return data;
+
+        }
+
+        private string priceProduct = "select Price from product where id=@id;";
+
+        public double getPrice(int id)
+        {
+            string aux = "0";
+            double price = 0;
+            try
+            {
+                command = new MySqlCommand(priceProduct, connection);
+                command.Parameters.AddWithValue("id", id);
+
+                connection.Open();
+
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    aux = Convert.ToString(reader["Price"]);
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                System.Console.WriteLine("Erro " + ex.Message.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+            price = Convert.ToDouble(aux);
+            return price;
         }
 
         private string addEmployer = "insert into employer(PersonID, Name) values (@personid, @name)";
@@ -75,7 +140,7 @@ namespace CaixaDeMercado
         {
             try
             {
-                command = new MySqlCommand(addEmployer, connection);   
+                command = new MySqlCommand(addEmployer, connection);
                 command.Parameters.AddWithValue("@personid", personid);
                 command.Parameters.AddWithValue("@name", name);
                 connection.Open();
@@ -83,17 +148,20 @@ namespace CaixaDeMercado
             }
             catch (MySql.Data.MySqlClient.MySqlException ex)
             {
-                System.Console.WriteLine("Erro " +ex.Message.ToString());   
+                System.Console.WriteLine("Erro " + ex.Message.ToString());
             }
             finally
             {
+                System.Console.WriteLine("Funcionario cadastrado com sucesso!");
                 connection.Close();
             }
         }
 
-        private string checkEmployer = "select * from employer where PersonID = @personid";
-        public MySqlDataReader readerEmployer(int personid)
+        private string checkEmployer = "select Name from employer where PersonID = @personid";
+
+        public string readerEmployer(int personid)
         {
+            string name = "invalid";
             try
             {
                 command = new MySqlCommand(checkEmployer, connection);
@@ -102,14 +170,52 @@ namespace CaixaDeMercado
                 connection.Open();
 
                 reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    name = Convert.ToString(reader["Name"]);
+                }
             }
             catch (MySql.Data.MySqlClient.MySqlException ex)
             {
-                System.Console.WriteLine("Erro " +ex.Message.ToString());   
+                System.Console.WriteLine("Erro " + ex.Message.ToString());
+            }
+            finally
+            {
+                connection.Close();
             }
 
-            return reader;
+            return name;
 
+        }
+
+        private string checkManager = "select Name from manager where PersonID = @personid";
+
+        public string readerManager(int personid)
+        {
+            string name = "invalid";
+            try
+            {
+                command = new MySqlCommand(checkManager, connection);
+                command.Parameters.AddWithValue("@personid", personid);
+
+                connection.Open();
+
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    name = Convert.ToString(reader["Name"]);
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                System.Console.WriteLine("Erro " + ex.Message.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return name;
         }
 
     }
